92bc0433d83dfb9e0c40a95ba5be8f79244b9d64
rainier <rmorenocueva@gmail.com>
2026-05-23 22:49:11 -0500

    archivo para eliminar tablas

--- sql/bootstrap/drop_tables.sql
+++ sql/bootstrap/drop_tables.sql
@@ -1,3 +1,6 @@
+-- ============================================================================
+-- ELIMINAR TABLAS BRONZE:
+-- ============================================================================
 DROP TABLE dev.bronze.raw_taxi_trip_yellow;
 DROP TABLE dev.bronze.raw_taxi_trip_green;
 DROP TABLE dev.bronze.raw_taxi_trip_zone;
