66e87341be907ac163084dac2236e8ed364aab28
rainier <rmorenocueva@gmail.com>
2026-05-30 19:42:35 -0500

    descriptivas del vendor_id

--- data/processed/local_transformer.py
+++ data/processed/local_transformer.py
@@ -37,20 +37,24 @@ class LocalTransformer:
         """
         columns=["id", "group_code", "group_description", "code", "description"]
         values = [
-            [1, "001001", "RatecodeID", 1, "Standard rate"],
-            [2, "001002", "RatecodeID", 2, "JFK"],
-            [3, "001003", "RatecodeID", 3, "Newark"],
-            [4, "001004", "RatecodeID", 4, "Nassau or Westchester"],
-            [5, "001005", "RatecodeID", 5, "Negotiated fare"],
-            [6, "001006", "RatecodeID", 6, "Group ride"],
-            [7, "001007", "RatecodeID", 99, "Null/unknown"],
+            [1, "001001", "rate_code_id", 1, "Standard rate"],
+            [2, "001002", "rate_code_id", 2, "JFK"],
+            [3, "001003", "rate_code_id", 3, "Newark"],
+            [4, "001004", "rate_code_id", 4, "Nassau or Westchester"],
+            [5, "001005", "rate_code_id", 5, "Negotiated fare"],
+            [6, "001006", "rate_code_id", 6, "Group ride"],
+            [7, "001007", "rate_code_id", 99, "Null/unknown"],
             [8, "002001", "payment_type", 0, "Flex Fare trip"],
             [9, "002002", "payment_type", 1, "Credit card"],
             [10, "002003", "payment_type", 2, "Cash"],
             [11, "002004", "payment_type", 3, "No charge"],
             [12, "002005", "payment_type", 4, "Dispute"],
             [13, "002006", "payment_type", 5, "Unknown"],
-            [14, "002007", "payment_type", 6, "Voided trip"]
+            [14, "002007", "payment_type", 6, "Voided trip"],
+            [15, "003001", "vendor_id", 1, "Creative Mobile Technologies, LLC"],
+            [16, "003002", "vendor_id", 2, "Curb Mobility, LLC"],
+            [17, "003003", "vendor_id", 6, "Myle Technologies Inc"],
+            [18, "003004", "vendor_id", 7, "Helix"]
         ]
         df = pd.DataFrame(columns=columns, data=values)
         df.to_csv(output_csv, index=False, sep=",")
